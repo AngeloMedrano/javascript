f9e5e868820b641bd74cb718345ef91c375ddb67
Angelo Medrano <angelitomijo1234@gmail.com>
2015-11-11 16:11:24 -0800

    added files

---
+++ day1/app01.js
@@ -0,0 +1,17 @@
+//Angelo Medrano 11/2/2015 Javascript 1
+var a = 12;
+var b = 0x1e;
+var c = "Answer equals: ";
+
+var abc = a + b;
+var answer = c + abc;
+console.log(answer);
+
+var n1 = 12;
+var n2 = 0x1e;
+var string = "Answer equals: ";
+
+var n3 = n1 + n2;
+var answer = string + n3;
+
+alert("I'm external");
---
+++ day1/myjsfile.js
@@ -0,0 +1,18 @@
+
+
+
+var character = prompt("Enter A, B, or C");
+
+switch(character){
+	case "A":
+		alert("You've entered A!");
+		break;
+	case "B":
+		alert("You've entered B!");
+		break;
+	case "C":
+		alert("You've entered C!");
+		break;
+	default:
+		alert("No proper response!");
+	};
